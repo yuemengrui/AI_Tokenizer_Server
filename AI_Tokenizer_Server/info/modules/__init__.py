68935eaa6772456ea56ec4b3716548ccca6101bb
yuemengrui <1401190335@qq.com>
2024-01-09 16:52:05 +0800

    add docker healthcheck

--- AI_Tokenizer_Server/info/modules/__init__.py
+++ AI_Tokenizer_Server/info/modules/__init__.py
@@ -1,7 +1,8 @@
 # *_*coding:utf-8 *_*
 from fastapi import FastAPI
-from . import word_seg
+from . import word_seg, health
 
 
 def register_router(app: FastAPI):
     app.include_router(router=word_seg.router, prefix="", tags=["Word Segmentation"])
+    app.include_router(router=health.router, prefix="", tags=["health"])
